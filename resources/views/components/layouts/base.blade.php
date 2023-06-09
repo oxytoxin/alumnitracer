cf6e5171b7554da583acf15f2d9ecbf6634af025
oxytoxin <oxytoxinsgrace@gmail.com>
2023-06-09 23:07:30 +0800

    configure layout based from filamentphp

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -49,10 +49,7 @@
         const theme = localStorage.getItem('theme')
 
         if (
-            theme === 'dark' ||
-            (!theme &&
-                window.matchMedia('(prefers-color-scheme: dark)')
-                .matches)
+            theme === 'dark'
         ) {
             document.documentElement.classList.add('dark')
         }
