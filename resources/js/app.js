bfc27f1dded0117901c1aa8fde992582eda0c963
oxytoxin <oxytoxinsgrace@gmail.com>
2023-06-10 10:35:49 +0800

    configure layout based from filamentphp

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,28 +23,6 @@ Alpine.plugin(Tooltip)
 Alpine.store('sidebar', {
     isOpen: Alpine.$persist(true).as('isOpen'),
 
-    collapsedGroups: Alpine.$persist(null).as('collapsedGroups'),
-
-    groupIsCollapsed: function (group) {
-        return this.collapsedGroups.includes(group)
-    },
-
-    collapseGroup: function (group) {
-        if (this.collapsedGroups.includes(group)) {
-            return
-        }
-
-        this.collapsedGroups = this.collapsedGroups.concat(group)
-    },
-
-    toggleCollapsedGroup: function (group) {
-        this.collapsedGroups = this.collapsedGroups.includes(group)
-            ? this.collapsedGroups.filter(
-                (collapsedGroup) => collapsedGroup !== group,
-            )
-            : this.collapsedGroups.concat(group)
-    },
-
     close: function () {
         this.isOpen = false
     },
--- resources/views/components/brand.blade.php
+++ resources/views/components/brand.blade.php
@@ -1,1 +1,3 @@
-Lerry
+<div>
+    Lerry
+</div>
